2fd4a91f48cfb7a724894b06b0e45636bf3b0f98
leonardieuris <gino.leonardi@euris.it>
2021-10-11 16:29:01 +0200

    Initial commit

---
+++ EquazionePrimoGrado/CalcoloEquazione.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazionePrimoGrado
+{
+    public class CalcoloEquazione
+    {
+        public List<double> Esegui(int a, int b, int c)
+        {
+            var delta = Delta(a, b, c);
+            var result = new List<double>();
+
+            if (delta == 0)
+            {
+                var res = -b / 2 * a;
+                result.Add(res);
+            }
+            if (delta > 0)
+            {
+                var res1 = (-b - Math.Sqrt(delta)) / (2 * a);
+                var res2 = (-b + Math.Sqrt(delta)) / (2 * a);
+                result.Add(res1);
+                result.Add(res2);
+            }
+
+            return result;
+        }
+
+        private double Delta(int a, int b, int c) => ( b * b ) - (4 * a * c);
+
+        
+    }
+}
---
+++ EquazionePrimoGrado/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace EquazionePrimoGrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var calcolo = new CalcoloEquazione();
+            var result = calcolo.Esegui(14, 74, 3);
+
+            if (!result.Any())
+            {
+                Console.WriteLine("Non ci sono soluzioni");
+            }
+            else
+            {
+                foreach (var elemento in result)
+                {
+
+                    Console.WriteLine($"Questa è una soluzione: {elemento}");
+                
+                }
+            }
+
+            Console.WriteLine("Fine");
+            Console.ReadLine();
+        }
+    }
+}
